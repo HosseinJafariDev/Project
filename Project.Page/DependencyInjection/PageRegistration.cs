041a02e64971ba40559938f0c25a01210ff3c53d
hosein <hoseinjaf84ri84@gmail.com>
2026-07-18 14:30:55 +0330

    [FEAT] add PageRegistration.cs

---
+++ Project.Page/DependencyInjection/PageRegistration.cs
@@ -0,0 +1,9 @@
+namespace Project.Page.DependencyInjection;
+
+public static class PageRegistration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
