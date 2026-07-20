924e6f3831265ff4c3219ba33eeb877c54b59fda
hosein <hoseinjaf84ri84@gmail.com>
2026-07-20 17:07:29 +0330

    Add dependencyinjection

--- Project.Application/DependencyInjection/ApplicationRegistration.cs
+++ Project.Application/DependencyInjection/ApplicationRegistration.cs
@@ -1,4 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
+using Project.Application.UseCases.Auth.Login;
+using Project.Application.UseCases.Auth.Logout;
+using Project.Application.UseCases.Auth.Password;
+using Project.Application.UseCases.Auth.Register;
 
 namespace Project.Application.DependencyInjection;
 
@@ -6,6 +10,10 @@ public static class ApplicationRegistration
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddScoped<ILoginUseCase, LoginUseCase>();
+        services.AddScoped<ILogoutUseCase, LogoutUseCase>();
+        services.AddScoped<IForgotPasswordUseCase, ForgotPasswordUseCase>();
+        services.AddScoped<IRegisterUseCase, RegisterUseCase>();
         return services;
     }
 }
