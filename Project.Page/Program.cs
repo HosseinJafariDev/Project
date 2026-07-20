02c957d63a278497994aee2a74b4fb62cf36f9cd
hosein <hoseinjaf84ri84@gmail.com>
2026-07-20 17:06:33 +0330

    add Configuretion Identity

--- Project.Page/Program.cs
+++ Project.Page/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using Project.Application.DependencyInjection;
+using Project.Domain.Entities.Roles;
+using Project.Domain.Entities.Users;
 using Project.Infrastructure.DependencyInjection;
+using Project.Infrastructure.Persistence;
 using Project.Infrastructure.Persistence.Identity;
 using Project.Page.DependencyInjection;
 
@@ -8,8 +13,49 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddPage();
+
+builder.Services.AddIdentity<User, Role>(options =>
+    {
+        // Password
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+
+
+        // User
+        options.User.RequireUniqueEmail = false;
+
+        // Lockout
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+
+        // SignIn
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddEntityFrameworkStores<PageDbContext>()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    //اگر لاکین نکرده باشد هدایت میشود به این صفحه 
+    options.LoginPath = "/Auth/LoginAjax";
+    //اگر لاگین کرده باشه و دسرسی نداشته باشه میاد به این صفحه 
+    options.AccessDeniedPath = "/AccessDenied";
+});
+
+
 var app = builder.Build();
 
+app.UseCustomExceptionHandler();
 
 using (var scope = app.Services.CreateScope())
 {
