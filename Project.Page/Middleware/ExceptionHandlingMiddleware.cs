335dd61f46e80a20a944b2cccebf029265f20838
hosein <hoseinjaf84ri84@gmail.com>
2026-07-20 17:13:31 +0330

    [FEAT] Add ExceptionHandlingMiddleware.cs

---
+++ Project.Page/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,57 @@
+using Project.Application.Exceptions;
+using Project.Domain.Exceptions;
+
+namespace Project.Page.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (ApplicationLayerException ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+        catch (DomainException exception)
+        {
+            await WriteResponseAsync(context, StatusCodes.Status422UnprocessableEntity, exception.Message);
+        }
+        catch (Exception ex)
+        {
+            await WriteResponseAsync(context, StatusCodes.Status500InternalServerError, "Error Server");
+        }
+    }
+
+    public async Task HandleExceptionAsync(HttpContext context, ApplicationLayerException ex)
+    {
+        switch (ex)
+        {
+            case ConflictException:
+            {
+                await WriteResponseAsync(context, StatusCodes.Status409Conflict, ex.Message);
+                break;
+            }
+            case NotFoundException:
+            {
+                await WriteResponseAsync(context, StatusCodes.Status400BadRequest, ex.Message);
+                break;
+            }
+            case ValidationException:
+            {
+                await WriteResponseAsync(context, StatusCodes.Status400BadRequest, ex.Message);
+                break;
+            }
+        }
+    }
+
+
+    public Task WriteResponseAsync(HttpContext context, int statusCode, string message)
+    {
+        context.Response.StatusCode = statusCode;
+        context.Response.ContentType = "application/json";
+        return context.Response.WriteAsync(message);
+    }
+}
